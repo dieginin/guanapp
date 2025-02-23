dc93b080bd32cb51d6f95ec33a877ee095f0e5eb
dieginin <diegobalestra9@gmail.com>
2025-02-22 17:56:24 -0800

    feat: create text_fields.py

--- components/__init__.py
+++ components/__init__.py
@@ -6,4 +6,5 @@ from .buttons import (
     TertiaryButton,
 )
 from .text import RegularText, Subtitle, Title
+from .text_fields import AddressField, EmailField, NameField, NumberField, RegularField
 from .uix import BottomSheet, ButtonRow
---
+++ components/text_fields.py
@@ -0,0 +1,174 @@
+from typing import Any, Callable, Optional
+
+import flet as ft
+
+
+class __TextField(ft.TextField):
+    def __init__(
+        self,
+        value: Optional[Any] = None,
+        label: Optional[str] = None,
+        autofocus: Optional[bool] = None,
+        on_change: Optional[Callable] = None,
+        on_click: Optional[Callable] = None,
+        on_submit: Optional[Callable] = None,
+        on_focus: Optional[Callable] = None,
+        on_blur: Optional[Callable] = None,
+        width: Optional[int] = None,
+    ) -> None:
+        super().__init__()
+        self.value = value
+        self.label = label
+        self.autofocus = autofocus
+        self.on_change = on_change
+        self.on_click = on_click
+        self.on_submit = on_submit
+        self.on_focus = on_focus
+        self.on_blur = on_blur
+        self.width = width
+        self.border = ft.InputBorder.UNDERLINE
+        self.focused_border_width = 3
+
+
+class RegularField(__TextField):
+    def __init__(
+        self,
+        value: Optional[str] = None,
+        label: Optional[str] = None,
+        autofocus: Optional[bool] = None,
+        on_change: Optional[Callable] = None,
+        on_click: Optional[Callable] = None,
+        on_submit: Optional[Callable] = None,
+        on_focus: Optional[Callable] = None,
+        on_blur: Optional[Callable] = None,
+        width: Optional[int] = None,
+    ) -> None:
+        super().__init__(
+            value,
+            label,
+            autofocus,
+            on_change,
+            on_click,
+            on_submit,
+            on_focus,
+            on_blur,
+            width,
+        )
+        self.keyboard_type = ft.KeyboardType.TEXT
+
+
+class NameField(__TextField):
+    def __init__(
+        self,
+        value: Optional[str] = None,
+        label: Optional[str] = None,
+        autofocus: Optional[bool] = None,
+        on_change: Optional[Callable] = None,
+        on_click: Optional[Callable] = None,
+        on_submit: Optional[Callable] = None,
+        on_focus: Optional[Callable] = None,
+        on_blur: Optional[Callable] = None,
+        width: Optional[int] = None,
+    ) -> None:
+        super().__init__(
+            value,
+            label,
+            autofocus,
+            on_change,
+            on_click,
+            on_submit,
+            on_focus,
+            on_blur,
+            width,
+        )
+        self.autofill_hints = ft.AutofillHint.NAME
+        self.capitalization = ft.TextCapitalization.WORDS
+        self.input_filter = ft.InputFilter(r"^[A-Za-z'\.\-\s]+|$")
+        self.keyboard_type = ft.KeyboardType.NAME
+
+
+class AddressField(__TextField):
+    def __init__(
+        self,
+        value: Optional[str] = None,
+        label: Optional[str] = None,
+        autofocus: Optional[bool] = None,
+        on_change: Optional[Callable] = None,
+        on_click: Optional[Callable] = None,
+        on_submit: Optional[Callable] = None,
+        on_focus: Optional[Callable] = None,
+        on_blur: Optional[Callable] = None,
+        width: Optional[int] = None,
+    ) -> None:
+        super().__init__(
+            value,
+            label,
+            autofocus,
+            on_change,
+            on_click,
+            on_submit,
+            on_focus,
+            on_blur,
+            width,
+        )
+        self.autofill_hints = ft.AutofillHint.ADDRESS_CITY_AND_STATE
+        self.capitalization = ft.TextCapitalization.SENTENCES
+        self.input_filter = ft.InputFilter(r"^[A-Za-z0-9'\.\-\s\,]+|$")
+        self.keyboard_type = ft.KeyboardType.STREET_ADDRESS
+
+
+class NumberField(__TextField):
+    def __init__(
+        self,
+        value: Optional[str] = None,
+        label: Optional[str] = None,
+        autofocus: Optional[bool] = None,
+        on_change: Optional[Callable] = None,
+        on_click: Optional[Callable] = None,
+        on_submit: Optional[Callable] = None,
+        on_focus: Optional[Callable] = None,
+        on_blur: Optional[Callable] = None,
+        width: Optional[int] = None,
+    ) -> None:
+        super().__init__(
+            value,
+            label,
+            autofocus,
+            on_change,
+            on_click,
+            on_submit,
+            on_focus,
+            on_blur,
+            width,
+        )
+        self.input_filter = ft.InputFilter(r"^([0-9]*[.])?[0-9]+|$")
+        self.keyboard_type = ft.KeyboardType.NUMBER
+
+
+class EmailField(__TextField):
+    def __init__(
+        self,
+        value: Optional[str] = None,
+        label: Optional[str] = None,
+        autofocus: Optional[bool] = None,
+        on_change: Optional[Callable] = None,
+        on_click: Optional[Callable] = None,
+        on_submit: Optional[Callable] = None,
+        on_focus: Optional[Callable] = None,
+        on_blur: Optional[Callable] = None,
+        width: Optional[int] = None,
+    ) -> None:
+        super().__init__(
+            value,
+            label,
+            autofocus,
+            on_change,
+            on_click,
+            on_submit,
+            on_focus,
+            on_blur,
+            width,
+        )
+        self.autofill_hints = ft.AutofillHint.EMAIL
+        self.input_filter = ft.InputFilter(r"^[^\s@]+@([^\s@.,]+\.)+[^\s@.,]{2,}|$")
+        self.keyboard_type = ft.KeyboardType.EMAIL
